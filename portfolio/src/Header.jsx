b8bfbb3cf0b6471023128aec0e0fce2b8eaccac5
x4mv <contactsscu@gmail.com>
2024-04-22 10:44:19 -0400

    adding header

---
+++ portfolio/src/Header.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+const Header = () => {
+  return (
+    <>
+        <header className='bg-black border-b border-solid border-slate-700 p-4'>
+            <nav>
+                <ul className='text-slate-400 p-2 flex justify-center'>
+                    <li className='transition duration-300 delay-150 hover:delay-150 hover:text-white ml-8'>About me</li>
+                    <li className='transition duration-300 delay-150 hover:delay-150 hover:text-white ml-8'>Projects</li>
+                    <li className='transition duration-300 delay-150 hover:delay-150 hover:text-white ml-8'>Contact me</li>
+                </ul>
+            </nav>
+        </header>
+    </>
+  )
+}
+
+export default Header
